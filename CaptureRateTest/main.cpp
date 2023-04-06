ddf043b864c496dd293376b9f7ce201a03ca0c2d
robmikh <rob.mikh@outlook.com>
2023-04-06 16:02:03 -0700

    refactor

--- CaptureRateTest/main.cpp
+++ CaptureRateTest/main.cpp
@@ -8,6 +8,7 @@ namespace winrt
     using namespace Windows::Graphics::Capture;
     using namespace Windows::Graphics::DirectX;
     using namespace Windows::Graphics::DirectX::Direct3D11;
+    using namespace Windows::System;
 }
 
 namespace util
@@ -38,14 +39,29 @@ struct Options
     uint32_t IntervalInMs;
 };
 
+struct MonitorCaptureItemSource
+{
+    HMONITOR Monitor;
+};
+
+struct WindowCaptureItemSource
+{
+    HWND Window;
+};
+
+typedef std::variant<MonitorCaptureItemSource, WindowCaptureItemSource> CaptureItemSource;
+
 std::optional<Options> ParseOptions(int argc, wchar_t* argv[], bool& error);
 std::optional<util::WindowInfo> GetWindowToCapture(std::vector<util::WindowInfo> const& windows);
-winrt::GraphicsCaptureItem CreateCaptureItemFromMonitorIndex(uint32_t monitorIndex);
+std::optional<CaptureItemSource> CreateItemSourceFromSubject(CaptureSubject captureSubject);
+std::optional<CaptureItemSource> CreateCaptureSourceFromMonitorIndex(uint32_t monitorIndex);
+std::optional<CaptureItemSource> CreateCaptureSourceFromWindowTitleSearch(std::wstring const& titleQuery);
+winrt::GraphicsCaptureItem CreateCaptureItemFromSource(CaptureItemSource itemSource);
 
 int __stdcall wmain(int argc, wchar_t* argv[])
 {
     // Initialize COM
-    winrt::init_apartment(winrt::apartment_type::single_threaded);
+    winrt::init_apartment(winrt::apartment_type::multi_threaded);
 
     // Parse args
     bool error = false;
@@ -55,86 +71,99 @@ int __stdcall wmain(int argc, wchar_t* argv[])
         return error ? 1 : 0;
     }
     uint32_t interval = options->IntervalInMs;
+    auto sourceOpt = CreateItemSourceFromSubject(options->Subject);
+    if (!sourceOpt.has_value())
+    {
+        return 1;
+    }
+    auto source = sourceOpt.value();
 
     // Init D3D
     auto d3dDevice = util::CreateD3DDevice();
     auto dxgiDevice = d3dDevice.as<IDXGIDevice>();
     auto device = CreateDirect3DDevice(dxgiDevice.get());
 
-    // Create the DispatcherQueue
-    auto controller = util::CreateDispatcherQueueControllerForCurrentThread();
+    // Create our capture thread
+    auto controller = winrt::DispatcherQueueController::CreateOnDedicatedThread();
     auto queue = controller.DispatcherQueue();
 
-    // Get our GraphicsCaptureItem
-    auto item = std::visit(overloaded
-        {
-            [=](MonitorCaptureSubject const& subject) -> winrt::GraphicsCaptureItem { return CreateCaptureItemFromMonitorIndex(subject.MonitorIndex); },
-            [=](WindowCaptureSubject const& subject) -> winrt::GraphicsCaptureItem 
-            { 
-                auto windows = util::FindTopLevelWindowsByTitle(subject.TitleQuery);
-                if (windows.size() == 0)
-                {
-                    wprintf(L"No windows found!\n");
-                    return nullptr;
-                }
-
-                auto foundWindow = GetWindowToCapture(windows);
-                if (!foundWindow.has_value())
-                {
-                    return nullptr;
-                }
-                wprintf(L"Using window \"%s\"\n", foundWindow->Title.c_str());
-                return util::CreateCaptureItemForWindow(foundWindow->WindowHandle);
-            },
-        }, options->Subject);
-    if (item == nullptr)
-    {
-        return 1;
-    }
-
-    // Setup capture
-    auto framePool = winrt::Direct3D11CaptureFramePool::Create(
-        device, 
-        winrt::DirectXPixelFormat::B8G8R8A8UIntNormalized, 
-        1, 
-        item.Size());
-    auto session = framePool.CreateCaptureSession(item);
-
+    // Setup capture and timer
     winrt::Direct3D11CaptureFrame frame{ nullptr };
-    framePool.FrameArrived([&frame](auto&& sender, auto&&)
+    winrt::Direct3D11CaptureFramePool framePool{ nullptr };
+    winrt::GraphicsCaptureSession session{ nullptr };
+    winrt::DispatcherQueueTimer timer{ nullptr };
+    wil::shared_event captureThreadEvent(wil::EventOptions::None);
+    queue.TryEnqueue([captureThreadEvent, &framePool, &session, &frame, &timer, device, source, interval, queue]()
         {
-            frame = sender.TryGetNextFrame();
-        });
-    session.StartCapture();
+            auto item = CreateCaptureItemFromSource(source);
+            framePool = winrt::Direct3D11CaptureFramePool::Create(
+                device,
+                winrt::DirectXPixelFormat::B8G8R8A8UIntNormalized,
+                1,
+                item.Size());
+            session = framePool.CreateCaptureSession(item);
+            framePool.FrameArrived([&frame](auto&& sender, auto&&)
+                {
+                    frame = sender.TryGetNextFrame();
+                });
 
-    // Setup our timer
-    auto timer = queue.CreateTimer();
-    timer.Interval(std::chrono::milliseconds(interval));
-    timer.IsRepeating(true);
-    
-    timer.Tick([&frame](auto&&, auto&&)
-        {
-            if (frame != nullptr)
-            {
-                frame.Close();
-            }
+            timer = queue.CreateTimer();
+            timer.Interval(std::chrono::milliseconds(interval));
+            timer.IsRepeating(true);
+
+            timer.Tick([&frame](auto&&, auto&&)
+                {
+                    if (frame != nullptr)
+                    {
+                        frame.Close();
+                    }
+                });
+            timer.Start();
+
+            session.StartCapture();
+            captureThreadEvent.SetEvent();
         });
-    timer.Start();
+    captureThreadEvent.wait();
 
     // Wait
-    wprintf(L"Press CTRL+C to exit...\n");
+    wprintf(L"Press ENTER to exit...\n");
+    std::wstring tempString;
+    std::getline(std::wcin, tempString);
 
-    // Message pump
-    MSG msg = {};
-    while (GetMessageW(&msg, nullptr, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessageW(&msg);
-    }
-    return util::ShutdownDispatcherQueueControllerAndWait(controller, static_cast<int>(msg.wParam));
+    captureThreadEvent.ResetEvent();
+    queue.TryEnqueue([timer, framePool, session, captureThreadEvent]()
+        {
+            timer.Stop();
+            framePool.Close();
+            session.Close();
+            captureThreadEvent.SetEvent();
+        });
+    captureThreadEvent.wait();
+    wprintf(L"Shutting down capture thread...\n");
+    controller.ShutdownQueueAsync().get();
+
+    return 0;
 }
 
-winrt::GraphicsCaptureItem CreateCaptureItemFromMonitorIndex(uint32_t monitorIndex)
+winrt::GraphicsCaptureItem CreateCaptureItemFromSource(CaptureItemSource itemSource)
+{
+    return std::visit(overloaded
+        {
+            [=](MonitorCaptureItemSource const& source) -> winrt::GraphicsCaptureItem { return util::CreateCaptureItemForMonitor(source.Monitor); },
+            [=](WindowCaptureItemSource const& source) -> winrt::GraphicsCaptureItem { return util::CreateCaptureItemForWindow(source.Window); },
+        }, itemSource);
+}
+
+std::optional<CaptureItemSource> CreateItemSourceFromSubject(CaptureSubject captureSubject)
+{
+    return std::visit(overloaded
+        {
+            [=](MonitorCaptureSubject const& subject) -> std::optional<CaptureItemSource> { return CreateCaptureSourceFromMonitorIndex(subject.MonitorIndex); },
+            [=](WindowCaptureSubject const& subject) -> std::optional<CaptureItemSource> { return CreateCaptureSourceFromWindowTitleSearch(subject.TitleQuery); },
+        }, captureSubject);
+}
+
+std::optional<CaptureItemSource> CreateCaptureSourceFromMonitorIndex(uint32_t monitorIndex)
 {
     std::vector<HMONITOR> monitors;
     winrt::check_bool(EnumDisplayMonitors(nullptr, nullptr, [](HMONITOR hmon, HDC, LPRECT, LPARAM lparam)
@@ -147,13 +176,31 @@ winrt::GraphicsCaptureItem CreateCaptureItemFromMonitorIndex(uint32_t monitorInd
 
     if (monitorIndex < monitors.size())
     {
-        return util::CreateCaptureItemForMonitor(monitors[monitorIndex]);
+        return std::optional(CaptureItemSource(MonitorCaptureItemSource{ monitors[monitorIndex] }));
     }
     else
     {
         wprintf(L"Monitor index is out of bounds!\n");
-        return nullptr;
+        return std::nullopt;
     }
+}
+
+std::optional<CaptureItemSource> CreateCaptureSourceFromWindowTitleSearch(std::wstring const& titleQuery)
+{
+    auto windows = util::FindTopLevelWindowsByTitle(titleQuery);
+    if (windows.size() == 0)
+    {
+        wprintf(L"No windows found!\n");
+        return std::nullopt;
+    }
+
+    auto foundWindow = GetWindowToCapture(windows);
+    if (!foundWindow.has_value())
+    {
+        return std::nullopt;
+    }
+    wprintf(L"Using window \"%s\"\n", foundWindow->Title.c_str());
+    return std::optional(CaptureItemSource(WindowCaptureItemSource{ foundWindow->WindowHandle }));
 }
 
 std::optional<util::WindowInfo> GetWindowToCapture(std::vector<util::WindowInfo> const& windows)
